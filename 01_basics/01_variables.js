1edb7d07cecef0a3da1631d5f8ed6c6d92881f61
Abhik-k <112130293+Abhik-k@users.noreply.github.com>
2024-01-03 17:30:28 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accName="Abhishek";
+
+let accId="12345";
+
+var accCity="Pune"; //Not use var because of issue in block scope and functional scope
+
+accHolder="Infy";
+
+let accState; //undefinded
+
+console.log(accId);
+
+console.table([accName,accId,accCity,accHolder,accState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treats all JS code as newer version
+
+//  alert(3+3) //will not work here as we are using nodejs not browser
+
+//number-- range 2^53 - 1
+//null-- standalone value
+//undefined--value hasn't assigned yet
+//symbol--to find uniqueness
+
+console.log(typeof "Abhi");
+
+console.log(typeof undefined);//undefined
+
+console.log(typeof null);//object
